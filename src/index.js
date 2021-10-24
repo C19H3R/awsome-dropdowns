26b6c2407f9784312d78fa7938af669446b5e442
C19H3R <karan1688.cse18@chitkara.edu.in>
2021-10-25 01:26:40 +0530

    added various other types of dropdowns

--- src/index.js
+++ src/index.js
@@ -1,20 +1,45 @@
-const getAllDropdowns = [...document.getElementsByClassName('black-awsome-dropdown')];
+const getAllDropdowns = [...document.getElementsByClassName("awsome-dropdown")];
 getAllDropdowns.forEach((item) => {
-  const divs = item.getElementsByTagName('div');
-  divs[0].addEventListener('click', () => {
-    item.classList.toggle('clicked');
+  const divs = item.getElementsByTagName("div");
+  divs[0].addEventListener("click", () => {
+    item.classList.toggle("clicked");
   });
-  const unorderedList = [...divs[1].getElementsByTagName('ul')];
-  if (divs[1].offsetWidth > divs[0].offsetWidth) { divs[0].style.minWidth = `${item.offsetWidth + 60}px`; }
-  const options = [...unorderedList[0].getElementsByTagName('li')];
+  const unorderedList = [...divs[1].getElementsByTagName("ul")];
+  if (divs[1].offsetWidth + 50 > divs[0].offsetWidth) {
+    divs[0].style.minWidth = `${divs[1].offsetWidth}px`;
+  }
+  // divs[0].style.width = divs[0].style.minWidth;
+  const options = [...unorderedList[0].getElementsByTagName("li")];
   options.forEach((opt) => {
-    opt.addEventListener('click', () => {
+    opt.addEventListener("click", () => {
       const value = opt.textContent;
-      item.dispatchEvent(new Event('change'));
-      item.setAttribute('value', value);
-      item.classList.toggle('clicked');
-      const optionSpan = divs[0].getElementsByTagName('span')[0];
+      item.dispatchEvent(new Event("change"));
+      item.setAttribute("value", value);
+      item.classList.toggle("clicked");
+      const optionSpan = divs[0].getElementsByTagName("span")[0];
       optionSpan.textContent = value;
     });
   });
+
+  // SPECIFIC STUFF
+
+  // DISCO
+  // if (item.className.includes("disco")) {
+  //   const RainBowColors = [
+  //     "violet",
+  //     "indigo",
+  //     "blue",
+  //     "green",
+  //     "yellow",
+  //     "orange",
+  //     "red",
+  //   ];
+  //   options.forEach((o) => {
+  //     const currentOption = o;
+  //     currentOption.style.background =
+  //       RainBowColors[parseInt(Math.random() * 6, 10) + 1];
+  //     currentOption.style.color = "black";
+  //     // o.setAttribute("style", `background: ${getRandomColor()} ;`);
+  //   });
+  // }
 });
