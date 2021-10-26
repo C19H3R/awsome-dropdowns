3cd27b99b27ccaabef081405cc254dc40c00a4a9
C19H3R <karan1688.cse18@chitkara.edu.in>
2021-10-26 22:09:16 +0530

    webpack  config file  update

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "main.min.js",
+    filename: "awsome-dropdowns.min.js",
     path: path.resolve(__dirname, "dist"),
   },
 };
